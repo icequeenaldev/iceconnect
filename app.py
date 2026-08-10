e28d20793cec3de417437169c4fddac0543042cc
icequeenaldev <icequeenal.dev@gmail.com>
2026-08-10 20:53:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1154,7 +1154,7 @@ def profile(username):
     <html><head><title>''' + user.username + ''''s Profile</title>
     <style>
     body{font-family:Arial;background:#0b1a2e;color:white;margin:0;padding:0 0 90px 0;}
-    .container{width:100%;max-width:600px;margin:auto;padding:12px;box-sizing:border-box;}
+    .container{padding:15px;width:100%;max-width:600px;margin:auto;box-sizing:border-box;}
     
     /* Profile Card */
     .card{background:#1a2a3e;padding:30px;border-radius:20px;position:relative;}
