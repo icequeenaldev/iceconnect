ae62a8a50155c604aed6da397f0a02c708a13e8d
icequeenaldev <icequeenal.dev@gmail.com>
2026-09-17 12:50:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1437,8 +1437,7 @@ def profile(username):
         # Follow
         if 'follow' in request.form:
     if is_blocked(current_user.id, user.id):
-        return redirect(url_for('profile', username=username))
-          if 'follow' in request.form: 
+        return redirect(url_for('profile', username=username)) 
             existing = Follow.query.filter_by(follower=current_user.username, followed=user.username).first()
             if not existing and current_user.username != user.username:
                 db.session.add(Follow(follower=current_user.username, followed=user.username))
